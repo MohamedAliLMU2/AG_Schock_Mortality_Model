5db4b2f6b4d2dd6bc1ec0ebe75ba25635c115245
MohamedAliLMU2 <mohamed.ali@campus.lmu.de>
2025-02-12 15:06:39 +0100

    Update AG_Schock_24h_Mortality.py

--- AG_Schock_24h_Mortality.py
+++ AG_Schock_24h_Mortality.py
@@ -80,12 +80,11 @@ else:
 
 if 'agreed' in st.session_state:
 
-    
+    #st.title("AG Schock Mortality Modell")
+
     
     st.markdown(
-        """
-        This website and its pre-trained models are designed exclusively for scientific research and should be used only for the prospective validation of the models. Any application of this tool for predicting patient mortality in real-world clinical settings or for guiding medical interventions is strictly prohibited. The authors assume no responsibility for any decisions, actions, or consequences resulting from the use or misuse of this tool. Users acknowledge that this tool is not a substitute for professional medical judgment and should not be relied upon for clinical decision-making.    
-    
+        """    
         The models have been pre-trained on data from cardiogenic shock patients, specifically those classified as SCAI stage C or higher.
     
         ### Simplified inclusion criteria:
